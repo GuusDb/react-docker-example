ca818eacb50e8c9ecb9be93b3e89a9fc97c22ce8
GuusDeBruyn <de.bruyn.guus@gmail.com>
2023-01-28 22:09:57 +0100

    init

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
+          <p>{`hello ${process.env.REACT_APP_NAME} fdfzdfdz`}</p>
           Learn React
         </a>
       </header>
